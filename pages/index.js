1ad1b1d589afeb4db4d986508afdf63d3dc77144
MaatheusJr27 <113740819+MaatheusJr27@users.noreply.github.com>
2026-06-12 17:12:28 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home () {
-    return <h1>Raquel vai casar comigo!</h1>
+function Home() {
+  return <h1>Raquel vai casar comigo!</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
